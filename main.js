1f151545b51a26083d98712b2ddeb39fb99635b7
zmd <zmd12356@qq.com>
2019-11-08 21:57:33 +0800

    添加handlebars，基本目录框架和主页的渲染

--- main.js
+++ main.js
@@ -1,12 +1,47 @@
 console.log('server launching...')
 
+const constant = require('./lib/constant')
 const express = require('express')
 const app = express()
 
+// 基础设置
+// process.env.NODE_ENV === "production"    //此模式下会自动开启缓存功能
+// app.enable('view cache');    //启用模板缓存
+// app.set('view cache', true)  //启用模板缓存
+app.disable('x-powered-by') //禁止发送服务器环境信息
+app.set('port', 8888)
+
+// 模板引擎两种使用方式
+// 方式一
+var handlebars = require('express-handlebars')
+app.engine('handlebars', handlebars());
+// 方式二
+// var handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
+// app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// 静态文件
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (request, response) => {
-    response.send('my-appstore')
+    response.render('home')
 })
 
-app.listen(8888, () => {
-    console.log('server listening on port 8888')
+// 404处理放在所有路由之后
+app.use((request, response) => {
+    response.type('text/plain')
+    response.status(404)
+    response.send('404 - not found')
+})
+
+// 错误处理放在最后
+app.use((error, request, response, next) => {
+    console.error(error.stack)
+    response.type('text/plain')
+    response.status(500)
+    response.send('500 - internal error')
+})
+
+app.listen(app.get('port'), () => {
+    console.log('server listening on port : ' + app.get('port'))
 })
