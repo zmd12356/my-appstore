99b540704d7a9ee6cc6ad6d5d0379747416b6729
zmd <zmd12356@qq.com>
2019-12-09 19:15:12 +0800

    默认使用双引号定义字符串

--- main.js
+++ main.js
@@ -1,56 +1,62 @@
-console.log('server launching...')
+console.log("server launching...")
 
-const constant = require('./lib/constant')
-const express = require('express')
-const fs = require('fs')
+const constant = require("./lib/constant")
+const express = require("express")
+const fs = require("fs")
 const app = express()
 
 // 基础设置
 // process.env.NODE_ENV === "production"    //此模式下会自动开启缓存功能
-// app.enable('view cache');    //启用模板缓存
-// app.set('view cache', true)  //启用模板缓存
-app.disable('x-powered-by') //禁止发送服务器环境信息
-app.set('port', 8888)
+// app.enable("view cache");    //启用模板缓存
+// app.set("view cache", true)  //启用模板缓存
+app.disable("x-powered-by") //禁止发送服务器环境信息
+app.set("port", 8888)
 
 // 模板引擎两种使用方式
 // 方式一
-var handlebars = require('express-handlebars')
-app.engine('handlebars', handlebars());
+var handlebars = require("express-handlebars")
+app.engine("handlebars", handlebars());
 // 方式二
-// var handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
-// app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+// var handlebars = require("express-handlebars").create({ defaultLayout: "main" })
+// app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
 
 // 静态文件
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"))
 
-app.get('/', (request, response) => {
-    var appsDirPath = './apps'
+var appsDirPath = "./apps"  //应用目录
+app.get("/", (request, response) => {
+
     fs.readdir(appsDirPath, (err, fileList) => {
+        var json = []
         if (err) {
             console.log(err)
         } else {
             console.log(fileList)
+            fileList.forEach(element => {
+
+                json.push({ "name": element })
+            });
         }
-        response.render('home')
+        response.render("home", { "appsInfo": json })
     })
 })
 
 // 404处理放在所有路由之后
 app.use((request, response) => {
-    response.type('text/plain')
+    response.type("text/plain")
     response.status(404)
-    response.send('404 - not found')
+    response.send("404 - not found")
 })
 
 // 错误处理放在最后
 app.use((error, request, response, next) => {
     console.error(error.stack)
-    response.type('text/plain')
+    response.type("text/plain")
     response.status(500)
-    response.send('500 - internal error')
+    response.send("500 - internal error")
 })
 
-app.listen(app.get('port'), () => {
-    console.log('server listening on port : ' + app.get('port'))
+app.listen(app.get("port"), () => {
+    console.log("server listening on port : " + app.get("port"))
 })
